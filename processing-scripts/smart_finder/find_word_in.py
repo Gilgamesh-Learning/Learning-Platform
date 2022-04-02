88340cb58c689bb259c1ab2d36cae74f58529cdb
Atanas Dimitrov <nasko119@abv.bg>
2022-04-03 00:17:38 +0300

    Add algorithm for finding good sentance matches in the extracted text

--- processing-scripts/smart_finder/find_word_in.py
+++ processing-scripts/smart_finder/find_word_in.py
@@ -2,6 +2,8 @@ import json
 import spacy
 import argparse
 import os
+import numpy as np
+import math
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Transcribe an audio file')
@@ -12,12 +14,54 @@ if __name__ == '__main__':
 
     nlp = spacy.load('en_core_web_lg')
 
-    all_sents = json.load(open(os.path.relpath(args.input)))
+    all_sents = json.load(open(os.path.relpath(args.input)))['split_info']
+
+    input_word = nlp(args.input_word)[0]
+    input_word_vector = input_word.vector / (np.linalg.norm(input_word.vector) + 1e-16)
+
+    words = []
+
+    for sent in all_sents:
+        sent['text'] = sent['text'].lower()
+
+    simil_with_input = lambda x: np.sum(np.multiply(x.vector, input_word_vector)) / (np.linalg.norm(x.vector) + 1e-16)
+
+    def remove_duplicate_tokens(tokens):
+        ret_tokens = []
+
+        for token in tokens:
+            cnt = 0
+            for other in tokens:
+                if other.text == token.text:
+                    cnt += 1
+                if cnt > 2:
+                    break
+            if cnt == 1:
+                ret_tokens.append(token)
+
+        return ret_tokens
+
+    def get_val_from_sent(sent):
+        tokens = list(filter(lambda x: x.text != '.', nlp(sent['text'])))
+        tokens = remove_duplicate_tokens(tokens)
+
+        CNT_ITER = 4
+        if len(tokens) < CNT_ITER:
+            return 0
+
+        tokens.sort(key = lambda x: -simil_with_input(x))
+
+        sum = 0
+        for i in range(CNT_ITER):
+            sum += simil_with_input(tokens[i])
+
+        return sum
+
+    all_sents.sort(key = lambda x: get_val_from_sent(x))
+
+    for sent in all_sents:
+        print(sent, get_val_from_sent(sent))
 
     with open(args.output_times_json, 'w+') as f:
         f.write('{"timestamps": [{"start": 1200, "end": 1200}]}')
-
-#    for sent in all_sents:
-#        print(sent)
-
 
