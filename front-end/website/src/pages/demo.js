5536180f02987726e2b9e39be705415548239e9f
LuboNenov <liubomir.n.2019@feg.bg>
2022-04-02 22:44:45 +0300

    Updated Client Side architecture for easier use

---
+++ front-end/website/src/pages/demo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './demo.css';
+
+export default function Demo() {
+
+  return (
+    <Layout title="Demo" description="Drop file">
+
+        <App />
+
+    </Layout>
+  );
+}
