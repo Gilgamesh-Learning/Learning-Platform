3a4b209c01b9f3df3a947378b6a2e43643a0c218
Atanas Dimitrov <nasko119@abv.bg>
2022-04-03 03:05:51 +0300

    Fix audio missing on ffmpeg cut

--- processing-scripts/video_cutter/cut.py
+++ processing-scripts/video_cutter/cut.py
@@ -13,22 +13,23 @@ if __name__ == '__main__':
     in_file = ffmpeg.input(args.input)
     in_split = json.load(open(os.path.relpath(args.split_info)))['split_info']
 
-    output_file = in_file.trim(start = in_split[0]['start'], end = in_split[0]['end'])
-
-    for ind in range(1, len(in_split) - 1):
+    for ind in range(len(in_split)):
         vid = (
             in_file
-            .trim(start = in_split[ind]['start'], end = in_split[ind]['end'])
+            .trim(start = in_split[ind]['start'] / 1000, end = in_split[ind]['end'] / 1000)
             .setpts('PTS-STARTPTS')
         )
         aud = (
             in_file
-            .filter_('atrim', start = in_split[ind]['start'], end = in_split[ind]['end'])
+            .filter_('atrim', start = in_split[ind]['start'] / 1000, end = in_split[ind]['end'] / 1000)
             .filter_('asetpts', 'PTS-STARTPTS')
         )
 
         joined = ffmpeg.concat(vid, aud, v=1, a=1)
 
-        output_file = ffmpeg.concat(output_file, joined)
+        if ind == 0:
+            output_file = joined
+        else:
+            output_file = ffmpeg.concat(output_file, joined)
 
     output_file.output(args.output_video).run_async()
